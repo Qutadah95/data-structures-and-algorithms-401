1c50e4d96b9ff30c5cbb787ac071db6f984824b8
Qutadah Almomani <85875347+Qutadah95@users.noreply.github.com>
2021-10-11 01:36:47 +0300

    Update reverseArray.java

--- challenges/reverseArray.java
+++ challenges/reverseArray.java
@@ -20,7 +20,7 @@ public class reverseArray {
 
     public static void main(String[] args)
     {
-        int [] arr = {[1, 2, 3, 4, 5, 6};
+        int [] arr = {1, 2, 3, 4, 5, 6};
         reverse(arr, arr.length);
     }
 }
