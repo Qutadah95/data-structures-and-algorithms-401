b68b0264eabe3acee0bf61322d8031f11d0842b0
Qutadah95 <qutadaalmomani@yahoo.com>
2021-11-01 00:45:57 +0200

    max tree half done

--- challenges/trees/src/main/java/BinaryTree.java
+++ challenges/trees/src/main/java/BinaryTree.java
@@ -1,41 +1,69 @@
 import java.util.ArrayList;
 
 public class BinaryTree {
-    ArrayList<Object> preOrder=new ArrayList<>();
+    ArrayList <Object> preOrder=new ArrayList<>();
     public ArrayList<Object> preOrder(Node root){
         if(root!=null){
             preOrder.add(root.getValue());
-            preOrder(root.getLeftChild());
-            preOrder(root.getRightChild());
-
+            if (root.getLeftChild()!=null){
+                preOrder(root.getLeftChild());
+            }
+            if (root.getRightChild()!=null){
+                preOrder(root.getRightChild());
+            }
         }
         return preOrder;
     }
     ArrayList <Object> inOrder=new ArrayList<>();
     public ArrayList<Object> inOrder(Node root){
         if(root!=null){
-            inOrder(root.getLeftChild());
+            if (root.getLeftChild()!=null){
+                inOrder(root.getLeftChild());
+            }
             inOrder.add(root.getValue());
-            inOrder(root.getRightChild());
+
+            if (root.getRightChild()!=null){
+                inOrder(root.getRightChild());
+            }
         }
         return inOrder;
     }
     ArrayList <Object> postOrder=new ArrayList<>();
     public ArrayList<Object> postOrder(Node root){
         if(root!=null){
-            postOrder(root.getLeftChild());
-            postOrder(root.getRightChild());
+            if (root.getLeftChild()!=null){
+                postOrder(root.getLeftChild());
+            }
+            if (root.getRightChild()!=null){
+                postOrder(root.getRightChild());
+            }
             postOrder.add(root.getValue());
         }
         return postOrder;
     }
+    int max=0;
+    public int treeMax(Node root){
+        if(root!=null){
+            if ((int)root.getValue()>max){
+                max=(int)root.getValue();
+            }
+            if (root.getLeftChild()!=null){
+                treeMax(root.getLeftChild());
+            }
+            if (root.getRightChild()!=null){
+                treeMax(root.getRightChild());
+            }
+        }
+        return max;
+    }
+
 
     @Override
     public String toString() {
         return "BinaryTree{" +
-                "preOrder=" + preOrder +
-                ", inOrder=" + inOrder +
-                ", postOrder=" + postOrder +
+                "preOrderArray=" + preOrder +
+                ", inOrderArray=" + inOrder +
+                ", postOrderArray=" + postOrder +
                 '}';
     }
 }
