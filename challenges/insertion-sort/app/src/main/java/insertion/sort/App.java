8c60558fc69e3aee37e1b448f0a42f064852cbc4
Qutadah95 <qutadaalmomani@yahoo.com>
2021-11-16 13:59:19 +0200

    cc28

--- challenges/insertion-sort/app/src/main/java/insertion/sort/App.java
+++ challenges/insertion-sort/app/src/main/java/insertion/sort/App.java
@@ -19,7 +19,8 @@ public class App {
         System.out.println(Arrays.toString(insertionSort(array)));
         System.out.println("mergeSort\n");
         System.out.println(Arrays.toString(mergeSort(array)));
-
+        System.out.println("quickSort\n");
+        System.out.println(Arrays.toString(quickSort(array,0,5)));
 
 
 
@@ -90,6 +91,34 @@ public class App {
             k++;
         }
 
+    }
+    public static int[] quickSort(int[] array, int left, int right) {
+        if(left < right) {
+            int position  = partition(array, left, right);
+            quickSort(array, left, position  - 1);
+            quickSort(array, position  + 1, right);
+        }
+        return array;
+    }
+
+    public static int partition(int[] array, int left, int right) {
+        int pivot  = array[right];
+        int low = left - 1;
+        for(int i = left; i < right; i++) {
+            if(array[i] - pivot < 0) {
+                low++;
+                swap(array, i, low);
+            }
+        }
+
+        swap(array, right, low + 1);
+        return low + 1;
+    }
+
+    public static void swap(int[] array, int i, int low) {
+        int temp = array[i];
+        array[i] = array[low];
+        array[low] = temp;
     }
 
 }
